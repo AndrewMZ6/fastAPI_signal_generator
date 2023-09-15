b4208b7698b31ed0c80d20f6df02a5b50864e064
AndrewMZ6 <zhaa@tor.org>
2023-09-15 15:32:53 +0700

    v1.0.0
    Added OFDM with pilots

--- fastapiserv.py
+++ fastapiserv.py
@@ -54,24 +54,71 @@ def generate_ofdm_nopilots(fftsize_arg, NN):
     return ofdm_time
 
 
+def generate_ofdm_withpilots(fftsize_arg, NN):
+    gsize = 100   
+    fftsize = fftsize_arg
+    P = 200
+
+    K = fftsize - 2*gsize - 1 + 1  # -1 of central zero, and +1 of right guard
+
+    # indicies of all subcarriers
+    allCarriers = np.arange(K)
+
+
+    pilotStep = K//P
+
+    # indicies of pilots
+    pilotCarriers = allCarriers[::pilotStep]
+    pilotValue = 2 + 2j
+
+    pilotCarriers = np.hstack([pilotCarriers, np.array([allCarriers[-1]])])
+
+    P = P + 1
+
+    dataCarriers = np.delete(allCarriers, pilotCarriers)
+    
+    # bits per symbol
+    mu = NN
+
+    # number of payload bits per OFDM symbol
+    payLoadBits_per_OFDM = len(dataCarriers)*mu
+
+    # generate random bits
+    bits = np.random.randint(low=0, high=2, size=payLoadBits_per_OFDM)
+
+    # modulate
+    M = cp.modulation.QAMModem(2**mu)
+    modBits = M.modulate(bits)
+
+    # Put complex data to spectrum
+    symbol = np.zeros(K, dtype=complex)
+    symbol[pilotCarriers] = pilotValue
+    symbol[dataCarriers] = modBits
+
+    ofdmSymbol = np.concatenate([np.zeros(gsize, dtype=complex), symbol[:int(K/2)], np.zeros(1, dtype=complex), symbol[int(K/2):], np.zeros(gsize-1, dtype=complex)])
+
+    ofdmSymbolShifted = SHIFT(ofdmSymbol)
+
+    ofdm_time = IDFT(ofdmSymbolShifted)
+    return ofdm_time, (pilotCarriers, dataCarriers), bits
+
+
+
 @app.get("/OFDM/{fftsize}/{Modulation_order}")
-def read_root(fftsize,Modulation_order):
-	print(fftsize)
-	print(type(fftsize))
-	arr = generate_ofdm_nopilots(int(fftsize), int(Modulation_order))
-	arr_r = json.dumps(arr.real.tolist())
-	arr_im = json.dumps(arr.imag.tolist())
-	return {'answer':{'real':arr_r, 'imag':arr_im}}
+async def read_root(fftsize, Modulation_order):
+
+    arr = generate_ofdm_nopilots(int(fftsize), int(Modulation_order))
+    arr_r = json.dumps(arr.real.tolist())
+    arr_im = json.dumps(arr.imag.tolist())
+    return {'answer':{'real':arr_r, 'imag':arr_im}}
 
 
 
-@app.get("/cOFDM/{fftsize}/{Modulation_order}")
-def read_root(fftsize,Modulation_order):
-	print(fftsize)
-	print(type(fftsize))
-	arr = generate_ofdm_nopilots(int(fftsize), int(Modulation_order))
-	arr_r = json.dumps(arr.real.tolist())
-	arr_im = json.dumps(arr.imag.tolist())
-	tt = ''.join(map(str, arr.tolist()))
+@app.get("/pOFDM/{fftsize}/{Modulation_order}")
+async def read_root_p(fftsize, Modulation_order):
 
-	return tt
+    arr, a, c = generate_ofdm_withpilots(int(fftsize), int(Modulation_order))
+    arr_r = json.dumps(arr.real.tolist())
+    arr_im = json.dumps(arr.imag.tolist())
+
+    return {'answer':{'real':arr_r, 'imag':arr_im}}
