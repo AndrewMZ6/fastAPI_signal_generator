794f6b87dbe1d4fbf2567c5a312ec563466a5279
Andrew <andreika3981@gmail.com>
2023-09-20 00:34:56 +0700

    added ofdm with no pilots request

--- matlab_web_client.m
+++ matlab_web_client.m
@@ -1,3 +1,4 @@
+%% Load complex array to api and get FFT of it
 clc; close all; clearvars;
 
 bits = randi([0, 1], 100, 1);
@@ -18,4 +19,22 @@ y_matlab = fft(signal_to_load);
 
 figure;
     plot(abs(xcorr(y, y_matlab)));
-    title('ello');
+    title('correlation of matlab fft and fastapi fft');
+    
+    
+%% Acquire ofdm symbol without pilots
+close all; clearvars; clc;
+
+modulation_order = 16;
+fftsize = 1024;
+endpoint_url = 'http://127.0.0.1:8088/OFDM/';
+
+
+api_url = [endpoint_url, num2str(fftsize), '/', num2str(modulation_order)];
+response = cellfun(@str2double, jsondecode(webread(api_url)));
+
+
+figure;
+    plot(abs(fft(response)));
+    
+scatterplot(fft(response));
